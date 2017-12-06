5fca71da903d9f6b9ba8ffc6f67c9ba4fbb7d5ec
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-06 12:08:33 +0000

    [Charlotte] #1 Creates a Summary component which can be rendered from index.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Summary from './Summary';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World!</h1>
-        </header>
-        <p className="App-intro">
-          Welcome to our app.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Hello World!</h1>
+                </header>
+                <p className="App-intro">
+                    Welcome to our app.
+                </p>
+                <p className="User-Option">
+                    Click here to go to your Summary page.
+                    <Summary value={Summary}/>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Summary.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Summary extends Component {
+    render() {
+        return (<h2>Be Open Banking</h2>);
+    }
+}
+
+export default Summary;
