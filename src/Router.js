0715ba5e754b7c4be0e200b9241b4aff2f2d5fa2
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-06 12:57:18 +0000

    [Charlotte] #1 Adds in Router to help to set up the routing to different pages

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import Summary from "./Summary";
+
+<__Router>
+    const App = () => (
+    <div>
+        <Switch>
+            <Route path='/summary' component={Summary}/>
+        </Switch>
+    </div>
+    )
+
+</__Router>
