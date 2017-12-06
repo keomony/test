330f9270e9692452382024eac43f00221c3ea7c0
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-06 17:31:25 +0000

    [Char/Neil] #1 Added Account Name to Summary component

--- src/Summary.js
+++ src/Summary.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Summary extends Component {
     render() {
-        return (<h2>Be Open Banking</h2>);
+        return (
+            <div className="Summary">
+                <h2>Be Open Banking</h2>
+                <h3>Account Name</h3>
+            </div>
+        );
     }
 }
 
